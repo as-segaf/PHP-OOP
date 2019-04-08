5ea00a132a4d2473d4f21767998ed26ed85a1aeb
as-segaf <ziyanassegaf20@gmail.com>
2019-04-08 14:36:00 +0700

    intial commit

---
+++ class-property-method-object/index.php
@@ -0,0 +1,44 @@
+<?php 
+
+	//class
+	class laptop{
+
+		//property
+		var $pemilik;
+		var $merk;
+		var $warna;
+
+		//method
+		function hidupkan_laptop(){
+			return "pencet tombol hidup laptop ini";
+		}
+
+		function matikan_laptop(){
+			return "pencet tombol mati laptop ini";
+		}
+	}
+
+	//objek (proses intansiasi)
+	$laptop_saya = new laptop();
+
+	//set property
+	$laptop_saya->pemilik="saya sendiri";
+	$laptop_saya->merk="asus";
+	$laptop_saya->warna="hitam";
+
+	//tampilkan property
+	echo $laptop_saya->pemilik;
+	echo "<br>";
+	echo $laptop_saya->merk;
+	echo "<br>";
+	echo $laptop_saya->warna;
+	echo "<br>";
+
+	//tampilkan method
+	echo $laptop_saya->hidupkan_laptop();
+	echo "<br>";
+	echo $laptop_saya->matikan_laptop();
+
+
+
+ ?>
