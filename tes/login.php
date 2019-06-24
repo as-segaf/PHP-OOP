7785d954c5346a38e9a7e0c815be8dc2a29e98fb
as-segaf <ziyanassegaf20@gmail.com>
2019-06-24 13:27:39 +0700

    initial commit

---
+++ tes/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<form action="prosesLogin.php" method="post">
+			<table>
+				<tr>
+					<td>Username :</td>
+					<td><input type="text" name="username" required></td>
+				</tr>
+				<tr>
+					<td>Password :</td>
+					<td><input type="text" name="password" required></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" name="submit" > <a href="register.php">Buat akun</a></td>
+				</tr>
+			</table>
+		</form>
+	</body>
+</html>
